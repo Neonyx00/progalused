66579f54121d2676e46e49c5cdc1df0f22a999f5
Alec <alec.kruuse@voco.ee>
2022-10-21 01:43:43 -0700

    Arvutamine.

---
+++ math expressions.py
@@ -0,0 +1,32 @@
+"""Set of different math expressions and exercises"""
+
+num_a = 5
+num_b = 2
+
+# 1. Sum and difference
+
+sum = num_a + num_b
+print(f"{num_a} - {num_b} = {difference}")
+print("--------------------------")
+
+Division = num_a // num_b
+print(f"){num_a} // {num_b} = {division}f")
+print("--------------------------------")
+
+average = (num_a +num_b) / 2
+print(f")({num_a} + {num_b}) / {2} = {average}")
+print("----------------------------------------")
+
+multiply_numbers = num_a + num_b
+power = num_a ** num_b
+remainder = num_a % num_b
+print(f"){num_a} % {num_b} = {remainder}")
+print("----------------------------------------")
+
+radius = 3.5
+import math
+pindala = radius * radius * math.pi
+print(f"){radius} * {radius} = math.pi = {pindala}")
+print(round(38.48451000647496, 2))
+circle_area = round(38.48451000647496, 2)
+print("------------------------------------")
